0af4c78606b7c108d4a4e15a88618c6c8cd3214a
Dennis Ogiermann <termi-official@users.noreply.github.com>
2019-07-15 20:45:44 +0200

    Improved description of which files belong to which license.

--- frontend/js/loaders/OBJLoader.js
+++ frontend/js/loaders/OBJLoader.js
@@ -2,6 +2,34 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
+//Taken from the threejs examples.
+//Source: https://threejs.org/docs/#examples/en/loaders/OBJLoader
+//A copy of the license can be found below.
+/*
+ The MIT License
+
+Copyright © 2010-2019 three.js authors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
 var THREE = require('three');
 var JSZip  = require('jszip');
 
