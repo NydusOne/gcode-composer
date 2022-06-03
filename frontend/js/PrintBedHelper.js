56c819b1b63fec895557d6caffd394b9a8057f7a
Dennis Ogiermann <termi-official@users.noreply.github.com>
2022-06-03 19:58:06 +0200

    Fix breaking changes due to ThreeJS update.

--- frontend/js/PrintBedHelper.js
+++ frontend/js/PrintBedHelper.js
@@ -1,44 +1,48 @@
 var THREE = require('three');
 
-function PrintBedHelper( sizeX, sizeY, step, color ) {
-	color = new THREE.Color( color !== undefined ? color : 0x444444 );
+class PrintBedHelper extends THREE.LineSegments {
+	constructor( sizeX, sizeY, step, color ) {
+    //constructor( parameters ) {
+		// sizeX = parameters.sizeX
+		// sizeY = parameters.sizeY
+		// step  = parameters.step
+		// color = parameters.color
+        color = new THREE.Color( color !== undefined ? color : 0x444444 );
 
-	// color and vertex buffer
-	var vertices = [], colors = [];
-  var bufferOffset=0;
+        // color and vertex buffer
+        var vertices = [], colors = [];
+        var bufferOffset=0;
 
-	// create lines in x direction
-  for ( var x = 0; x < sizeX; x+=step) {
-		vertices.push( x, 0, 0, x, sizeY, 0 );
+        // create lines in x direction
+          for ( var x = 0; x < sizeX; x+=step) {
+            vertices.push( x, 0, 0, x, sizeY, 0 );
 
-		color.toArray( colors, bufferOffset ); bufferOffset += 3;
-		color.toArray( colors, bufferOffset ); bufferOffset += 3;
-	}
-  vertices.push( sizeX, 0, 0, sizeX, sizeY, 0 );
-  color.toArray( colors, bufferOffset ); bufferOffset += 3;
-  color.toArray( colors, bufferOffset ); bufferOffset += 3;
+            color.toArray( colors, bufferOffset ); bufferOffset += 3;
+            color.toArray( colors, bufferOffset ); bufferOffset += 3;
+        }
+        vertices.push( sizeX, 0, 0, sizeX, sizeY, 0 );
+        color.toArray( colors, bufferOffset ); bufferOffset += 3;
+        color.toArray( colors, bufferOffset ); bufferOffset += 3;
 
-	// create the lines in y direction
-  for ( var y = 0; y < sizeY; y+=step) {
-    vertices.push( 0, y, 0, sizeX, y, 0 );
+        // create the lines in y direction
+        for ( var y = 0; y < sizeY; y+=step) {
+            vertices.push( 0, y, 0, sizeX, y, 0 );
+            color.toArray( colors, bufferOffset ); bufferOffset += 3;
+            color.toArray( colors, bufferOffset ); bufferOffset += 3;
+        }
+        vertices.push( 0, sizeY, 0, sizeX, sizeY, 0  );
+        color.toArray( colors, bufferOffset ); bufferOffset += 3;
+        color.toArray( colors, bufferOffset ); bufferOffset += 3;
 
-		color.toArray( colors, bufferOffset ); bufferOffset += 3;
-		color.toArray( colors, bufferOffset ); bufferOffset += 3;
-	}
-  vertices.push( 0, sizeY, 0, sizeX, sizeY, 0  );
-  color.toArray( colors, bufferOffset ); bufferOffset += 3;
-  color.toArray( colors, bufferOffset ); bufferOffset += 3;
+        var geometry = new THREE.BufferGeometry();
+        geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+        geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
 
-	var geometry = new THREE.BufferGeometry();
-	geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-	geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+        var material = new THREE.LineBasicMaterial( { vertexColors: true } );
 
-	var material = new THREE.LineBasicMaterial( { vertexColors: THREE.VertexColors } );
+        super( geometry, material );
+    }
+};
 
-	THREE.LineSegments.call( this, geometry, material );
-}
-
-PrintBedHelper.prototype = Object.create( THREE.LineSegments.prototype );
-PrintBedHelper.prototype.constructor = PrintBedHelper;
-
+//export { PrintBedHelper };
 module.exports = PrintBedHelper;
